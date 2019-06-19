e0c77ff937915ea9eea3df5164f6a8014d1aa319
dengmiao <1297320571@qq.com>
2019-06-19 17:50:33 +0800

    首页

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 /*
 包含应用中所有请求函数的模块
  */
-
+import jsonp from 'jsonp'
+import {message} from 'antd'
 import ajax from './ajax'
 
 const BASE = ''
@@ -14,3 +15,40 @@ export const reqLogin = (username, password) => ajax(BASE + '/login', {username,
 
 // 注册
 export const reqAddUser = (user) => ajax(BASE + '/manager/user/add', user, 'POST')
+
+/*
+jsonp请求的接口函数
+ */
+export const reqWeather = (city) => {
+
+    return new Promise((resolve, reject) => {
+        const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=cun0jVgP73Fw0aOBCbLKZ4lob4LH1QaT`
+        // 发送jsonp请求
+        jsonp(url, {}, (err, data) => {
+            console.log('jsonp()', err, data)
+            // 如果成功了
+            if (!err && data.status === 'success') {
+                // 取出需要的数据
+                const {dayPictureUrl, weather} = data.results[0].weather_data[0]
+                resolve({dayPictureUrl, weather})
+            } else {
+                // 如果失败了
+                message.error('获取天气信息失败!')
+            }
+
+        })
+    })
+}
+/*
+jsonp解决ajax跨域的原理
+  1). jsonp只能解决GET类型的ajax请求跨域问题
+  2). jsonp请求不是ajax请求, 而是一般的get请求
+  3). 基本原理
+   浏览器端:
+      动态生成<script>来请求后台接口(src就是接口的url)
+      定义好用于接收响应数据的函数(fn), 并将函数名通过请求参数提交给后台(如: callback=fn)
+   服务器端:
+      接收到请求处理产生结果数据后, 返回一个函数调用的js代码, 并将结果数据作为实参传入函数调用
+   浏览器端:
+      收到响应自动执行函数调用的js代码, 也就执行了提前定义好的回调函数, 并得到了需要的结果数据
+ */
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,21 +1,95 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 
+import { reqWeather } from "../../api"
+import { formateDate } from '../../utils/dateUtils'
+import memoryUtils from '../../utils/memoryUtils'
+import menuList from '../../config/menuConfig'
 import './index.less'
 
 /*
 头部
  */
-export default class Header extends Component {
+class Header extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            // 当前时间的字符串
+            currentTime: formateDate(Date.now()),
+            // 天气图片url
+            dayPictureUrl: '',
+            // 天气文本
+            weather: '',
+        };
+    }
+
+    getTime = () => {
+        // 每隔1秒 获取当前时间 更新currentTime
+        setInterval(() => {
+            const currentTime = formateDate(Date.now())
+            this.setState({currentTime})
+        },1000)
+    }
+
+    getWeather = async () => {
+        // 调用接口请求异步获取数据
+        const {dayPictureUrl, weather} = await reqWeather('成都')
+        // 更新状态
+        this.setState({dayPictureUrl, weather})
+    }
+
+    getTitle = () => {
+        // 得到当前请求路径
+        const path = this.props.location.pathname
+        let title = ''
+        menuList.forEach((item) => {
+            if(item.key === path) {
+                title = item.title
+            } else if(item.children) {
+                const cItem = item.children.find(cItem => cItem.key === path)
+                if(cItem) {
+                    title = cItem.title
+                }
+            }
+        })
+        return title
+    }
+
+    /*
+    第一次render()之后执行一次
+    一般在此执行异步操作: 发ajax请求/启动定时器
+     */
+    componentDidMount() {
+        // 获取当前时间
+        this.getTime()
+        // 获取天气
+        // 40节
+        this.getWeather()
     }
 
     render() {
+        const { currentTime, dayPictureUrl, weather } = this.state
+        const { username } = memoryUtils.user
+
+        const title = this.getTitle()
+
         return (
             <div className='header'>
-                Header
+                <div className="header-top">
+                    <span>欢迎, { username }</span>
+                    {/*<a href="javascript: alert(11)">退出</a>*/}
+                </div>
+                <div className="header-bottom">
+                    <div className="header-bottom-left">{ title }</div>
+                    <div className="header-bottom-right">
+                        <span>{ currentTime }</span>
+                        <img src={ dayPictureUrl } alt="weather" />
+                        <span>{ weather }</span>
+                    </div>
+                </div>
             </div>
         )
     }
 }
+
+export default withRouter(Header)
